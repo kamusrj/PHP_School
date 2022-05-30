8ca61a5b03220db1de8f6304b6c708210b0bc278
kamusrj <tonyin_munoz@hotmail.com>
2022-05-30 16:40:44 -0600

    inicio de proyecto

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--
+ Kamus Curso java Insafor 2022
+-->
+<html>
+    <head>
+        <title>TODO supply a title</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+       <?php 
+       
+       echo "index";
+       ?>
+    </body>
+</html>
